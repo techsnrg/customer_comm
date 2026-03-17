fb8cbc41c4a7ec2477dc6b8a1a4af41500bb092e
Nikhil Goel <nikhil@Nikhils-MacBook-Air.local>
2026-03-18 02:54:33 +0530

    Fix setup.py formatting and add MANIFEST.in

--- setup.py
+++ setup.py
@@ -3,12 +3,12 @@ from setuptools import setup
 name = "customer_comm"
 
 setup(
-	name=name,
+	name="customer_comm",
 	version="0.0.1",
 	description="A unified customer journey system for ERPNext",
 	author="Nikhil Goel",
 	author_email="nikhil@email.com",
-	packages=[name],
+	packages=["customer_comm"],
 	zip_safe=False,
 	include_package_data=True,
 	install_requires=(
